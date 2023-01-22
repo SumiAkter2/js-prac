ae398c77ea6a725411023cc442d894a1eb887bff
SumiAkter2 <akteralisha36@gmail.com>
2023-01-22 21:22:05 +0600

    first commit

---
+++ task1.js
@@ -0,0 +1,34 @@
+// 1.
+var fruits = ["Apple", "Banana", "Orange"];
+// console.log(fruits.indexOf('Banana'));
+// fruits[1] = "Mango";
+// fruits.pop();
+// fruits.push("Watermelon");
+console.log(fruits);
+
+// 2.
+var firstNumber = 13;
+var secondNumber = 79;
+var thirdNumber = 45;
+const largestNumber = Math.max(firstNumber, secondNumber, thirdNumber);
+const lowestNumber = Math.min(firstNumber, secondNumber, thirdNumber);
+// console.log(largestNumber,lowestNumber);
+
+// 3.
+var x = 9;
+var y = 8;
+var z = 9;
+if (x == y || y == z || x == z) {
+  // console.log('isosceles');
+} else {
+  // console.log('not isosceles');
+}
+
+// 4.
+var firstLine = "I am going to be";
+var secondLine = "an awesome web developer";
+// console.log(firstLine +' '+ secondLine);
+
+// 5.
+var number = 119;
+// console.log(number%5);
