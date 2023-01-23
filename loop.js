f6e275d1c476c9e5ad62c5f8e6b1c463fb058607
SumiAkter2 <akteralisha36@gmail.com>
2023-01-23 16:05:36 +0600

    added loop js

---
+++ loop.js
@@ -0,0 +1,9 @@
+var number = 3;
+while (number<=100) {
+    // console.log(number);
+    number++;
+}
+for (let index = 0; index < 20; index++) {
+    // const element = array[index];
+    console.log(index);
+}
