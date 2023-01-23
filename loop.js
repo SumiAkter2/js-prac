8513d507fb836bb1bd47a340e441c31db8c6a1ee
SumiAkter2 <akteralisha36@gmail.com>
2023-01-23 17:02:57 +0600

    added break and continue in loop

--- loop.js
+++ loop.js
@@ -1,9 +1,38 @@
 var number = 3;
-while (number<=100) {
-    // console.log(number);
-    number++;
+while (number <= 100) {
+  // console.log(number);
+  number++;
 }
 for (let index = 0; index < 20; index++) {
-    // const element = array[index];
-    console.log(index);
+  //   console.log(index);
+}
+var i = 1;
+while (i < 40) {
+  // console.log('ajke amr mon valo nai', i);
+  i++;
+}
+for (var i = 1; i < 40; i++) {
+  //   console.log("ajke amr mon valo nai", i);
+}
+for (var i = 412; i < 456; i += 2) {
+  //   console.log( i);
+}
+var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+for (var i = 0; i < cars.length; i++) {
+  const element = cars[i];
+  // console.log(element);
+}
+for (var n = 30; n < 87; n++) {
+  if (element == 44) {
+    break;
+  }
+  //   console.log(n);
+}
+var roll = [122, 345, 200, 150, 400, 530, 176, 198];
+for (var l = 0; l < roll.length; l++){
+    var rol = roll[l]
+    if (rol >= 200) {
+        continue
+    }
+    console.log(rol);
 }
